3da57f8d4bdd7e7e3c8fabbaa26582c696a0d0e8
jamie <jamie@snowkiwi.com>
2016-11-25 15:38:17 +0900

    kitten

--- index-total.js
+++ index-total.js
@@ -7,6 +7,21 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.listen((process.env.PORT || 3000));
 
+var Forecast = require('forecast');
+// Initialize 
+var forecast = new Forecast({
+  service: 'darksky',
+  key: 'f0ad1b95c9fc6c9d0635c5b8a99f0b06',
+  units: 'celcius',
+  cache: true,
+  ttl: {
+    http://momentjs.com/docs/#/durations/creating/ 
+    minutes: 27,
+    seconds: 45
+  }
+});
+
+
 // Server frontpage
 app.get('/', function (req, res) {
     res.send('This is TestBot Server');
@@ -25,7 +40,7 @@ app.get('/forecast', function (req, res) {
 
 // Facebook Webhook
 app.get('/webhook', function (req, res) {
-    if (req.query['hub.verify_token'] === 'abcd_verify_token') {  // webhook 설정에 입력된 토큰
+    if (req.query['hub.verify_token'] === 'abcd_verify_token') {
         res.send(req.query['hub.challenge']);
     } else {
         res.send('Invalid verify token');
@@ -37,13 +52,29 @@ app.post('/webhook', function (req, res) {
     var events = req.body.entry[0].messaging;
     for (i = 0; i < events.length; i++) {
         var event = events[i];
+//      phase 1.
+//        if (event.message && event.message.text) {
+//            sendMessage(event.sender.id, {text: "Echo: " + event.message.text});
+//        }
+
+//      phase 2.
+//        if (event.message && event.message.text) {
+//            if (!kittenMessage(event.sender.id, event.message.text)) {
+//                sendMessage(event.sender.id, {text: "Echo: " + event.message.text});
+//            }
+//        } else if (event.postback) {
+//            console.log("Postback received: " + JSON.stringify(event.postback));
+//        }
+      
+//      phase 3.
         if (event.message && event.message.text) {
-            if (!kittenMessage(event.sender.id, event.message.text)) {
-                sendMessage(event.sender.id, {text: "Echo: " + event.message.text});
-            }
-        } else if (event.postback) {
-            console.log("Postback received: " + JSON.stringify(event.postback));
-        }    
+          addMessage(event.sender.id, event.message.text);
+        }
+      
+//      phase 4.
+//          if (event.message && event.message.text) {
+//              weatherMessage(event.sender.id, event.message.text);
+//          }       
     }
     res.sendStatus(200);
 });
@@ -110,4 +141,33 @@ function kittenMessage(recipientId, text) {
   return false;
     
 };
+
+// send rich message with kitten
+function addMessage(recipientId, text) {
+    
+  text = text || "";
+  var values = text.split(' ');
+
+  if (values.length === 3 && values[0] === 'add') {
+      if (Number(values[1]) > 0 && Number(values[2]) > 0) {
+          addText = values[1] + " + " + values[2] + " = " + (Number(values[1]) + Number(values[2]));
+          message = {text: addText};
+          sendMessage(recipientId, message);
+      }
+  }
+};
+
+function weatherMessage(recipientId, text){
+  text = text || "";
+  var values = text.split(' ');
+
+  if (values[0] === '날씨') {
+    forecast.get([35.9335, 139.6181], function(err, weather) {
+      console.log(weather);
+      weatherText = "오늘 " + weather.timezone + "의 날씨는 " + weather.currently.summary + "입니다. ";
+      message = {text: weatherText};
+      sendMessage(recipientId, message);
+    });
+  } 
+}
 
