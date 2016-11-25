1cca6676479ed7206977cb1285907647c2dba2f2
jamie <jamie@snowkiwi.com>
2016-11-25 16:02:29 +0900

    add bot

--- index.js
+++ index.js
@@ -12,10 +12,20 @@ app.get('/', function (req, res) {
     res.send('This is TestBot Server');
 });
 
+app.get('/forecast', function (req, res) {
+    forecast.get([35.9335, 139.6181], function(err, weather) {
+      if(err) return console.dir(err);
+      console.dir(weather);
+      console.dir(weather.latitude);
+      console.log("weather.latitude " + weather.latitude);
+      console.log("오늘 " + weather.timezone + "의 날씨는 " + weather.currently.summary + "입니다. ");
+    });
+    res.send('forecast');
+});
+
 // Facebook Webhook
 app.get('/webhook', function (req, res) {
-    console.log(req.query['hub.verify_token']);
-    if (req.query['hub.verify_token'] === 'abcd_verify_token') {  // webhook 설정에 입력된 토큰
+    if (req.query['hub.verify_token'] === 'abcd_verify_token') {
         res.send(req.query['hub.challenge']);
     } else {
         res.send('Invalid verify token');
@@ -28,12 +38,8 @@ app.post('/webhook', function (req, res) {
     for (i = 0; i < events.length; i++) {
         var event = events[i];
         if (event.message && event.message.text) {
-            if (!kittenMessage(event.sender.id, event.message.text)) {
-                sendMessage(event.sender.id, {text: event.message.text});
-            }
-        } else if (event.postback) {
-            console.log("Postback received: " + JSON.stringify(event.postback));
-        }    
+          addMessage(event.sender.id, event.message.text);
+        }   
     }
     res.sendStatus(200);
 });
@@ -57,47 +63,16 @@ function sendMessage(recipientId, message) {
     });
 };
 
-// send rich message with kitten
-function kittenMessage(recipientId, text) {
+function addMessage(recipientId, text) {
     
   text = text || "";
   var values = text.split(' ');
 
-  if (values.length === 3 && values[0] === 'kitten') {
+  if (values.length === 3 && values[0] === 'add') {
       if (Number(values[1]) > 0 && Number(values[2]) > 0) {
-
-          var imageUrl = "https://placekitten.com/" + Number(values[1]) + "/" + Number(values[2]);
-
-          message = {
-              "attachment": {
-                  "type": "template",
-                  "payload": {
-                      "template_type": "generic",
-                      "elements": [{
-                          "title": "Kitten",
-                          "subtitle": "Cute kitten picture",
-                          "image_url": imageUrl ,
-                          "buttons": [{
-                              "type": "web_url",
-                              "url": imageUrl,
-                              "title": "Show kitten"
-                              }, {
-                              "type": "postback",
-                              "title": "I like this",
-                              "payload": "User " + recipientId + " likes kitten " + imageUrl,
-                          }]
-                      }]
-                  }
-              }
-          };
-
+          addText = values[1] + " + " + values[2] + " = " + (Number(values[1]) + Number(values[2]));
+          message = {text: addText};
           sendMessage(recipientId, message);
-
-          return true;
       }
   }
-
-  return false;
-    
 };
-
